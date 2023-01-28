d4c20864c8be0f68a82dc514b08b05a8b52baa23
Ragdha El-gaidi <76912120+Ragdha-Elgaidi@users.noreply.github.com>
2023-01-28 05:44:49 +0200

    Update Practice(10).sql

--- Basic SQL/Practice(10).sql
+++ Basic SQL/Practice(10).sql
@@ -3,6 +3,6 @@
 -- You will need to use only the columns that end with _usd. (Try to do this without using the total column.) 
 -- Display the id and account_id fields also.
 SELECT id, account_id, 
-poster_amt_usd/(standard_amt_usd + gloss_amt_usd + poster_amt_usd) AS post_per
+poster_amt_usd/(standard_amt_usd + gloss_amt_usd + poster_amt_usd)*100 AS post_per
 FROM orders
 LIMIT 10;
