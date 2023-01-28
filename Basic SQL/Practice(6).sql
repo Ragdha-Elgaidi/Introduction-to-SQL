f3d8491e69fb2dbaedbbb18d989d165d1a7bcbbd
Ragdha El-gaidi <76912120+Ragdha-Elgaidi@users.noreply.github.com>
2023-01-28 05:11:55 +0200

    Create Practice(6).sql

---
+++ Basic SQL/Practice(6).sql
@@ -0,0 +1,5 @@
+#### Pulls the first 5 rows and all columns from the orders table that have a dollar amount of gloss_amt_usd greater than or equal to 1000.
+SELECT *
+FROM orders
+WHERE gloss_amt_usd >= 1000
+LIMIT 5;
