08cfd40e953eb153301a303a3de50397d0593156
Ragdha El-gaidi <76912120+Ragdha-Elgaidi@users.noreply.github.com>
2023-01-28 05:39:18 +0200

    Create Practice(9).sql

---
+++ Basic SQL/Practice(9).sql
@@ -0,0 +1,6 @@
+-- Using the orders table:
+-- Create a column that divides the standard_amt_usd by the standard_qty to find the unit price for standard paper for each order. Limit the results to the first 10 orders, and include the id and account_id fields.
+SELECT id, account_id, standard_amt_usd/standard_qty AS unit_price
+FROM orders
+LIMIT 10;
+
