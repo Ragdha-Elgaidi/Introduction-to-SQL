6df492fe1722623c04e5e412def9c2eb30b71c50
Ragdha El-gaidi <76912120+Ragdha-Elgaidi@users.noreply.github.com>
2023-01-30 02:55:54 +0200

    Create Practice(2).sql

---
+++ SQL Joins/Practice(2).sql
@@ -0,0 +1,7 @@
+-- Try pulling standard_qty, gloss_qty, and poster_qty from the orders table, and the website and the primary_poc from the accounts table. 
+SELECT orders.standard_qty, orders.gloss_qty, 
+ orders.poster_qty,  accounts.website, 
+ accounts.primary_poc
+FROM orders
+JOIN accounts
+ON orders.account_id = accounts.id
