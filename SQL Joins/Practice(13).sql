71dd908c404cc6511a4d899d0ece17934835be4b
Ragdha El-gaidi <76912120+Ragdha-Elgaidi@users.noreply.github.com>
2023-01-31 01:15:59 +0200

    Create Practice(13).sql

---
+++ SQL Joins/Practice(13).sql
@@ -0,0 +1,9 @@
+-- Find all the orders that occurred in 2015.
+-- Your final table should have 4 columns: occurred_at, account name, order total, and order total_amt_usd.
+
+SELECT o.occurred_at, a.name, o.total, o.total_amt_usd
+FROM accounts a
+JOIN orders o
+ON o.account_id = a.id
+WHERE o.occurred_at BETWEEN '01-01-2015' AND '01-01-2016'
+ORDER BY o.occurred_at DESC;
