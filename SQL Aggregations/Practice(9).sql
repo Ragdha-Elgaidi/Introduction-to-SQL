936fd74a7603f6770d070b453ac5f9bd68c5d0b3
Ragdha El-gaidi <76912120+Ragdha-Elgaidi@users.noreply.github.com>
2023-01-31 07:09:11 +0200

    Create Practice(9).sql

---
+++ SQL Aggregations/Practice(9).sql
@@ -0,0 +1,7 @@
+-- find the total sales in usd for each account. You should include two columns
+-- the total sales for each company's orders in usd and the company name.
+SELECT a.name, SUM(total_amt_usd) total_sales
+FROM orders o
+JOIN accounts a
+ON a.id = o.account_id
+GROUP BY a.name;
