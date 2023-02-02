d7679f7e78fdf4c60da362a569abeb00b9bab26d
Ragdha El-gaidi <76912120+Ragdha-Elgaidi@users.noreply.github.com>
2023-02-02 11:18:06 +0200

    Create Practice(34).sql

---
+++ SQL Aggregations/Practice(34).sql
@@ -0,0 +1,8 @@
+-- Write a query to display the number of orders in each of three categories, based on the total number of items in each order. 
+-- The three categories are: 'At Least 2000', 'Between 1000 and 2000' and 'Less than 1000'.
+SELECT CASE WHEN total >= 2000 THEN 'At Least 2000'
+WHEN total >= 1000 AND total < 2000 THEN 'Between 1000 and 2000'
+ELSE 'Less than 1000' END AS order_category,
+COUNT(*) AS order_count
+FROM orders
+GROUP BY 1;
