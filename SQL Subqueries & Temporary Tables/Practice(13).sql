bd4b89f9131f5d3be54ffef80a803bfde82ddec6
Ragdha El-gaidi <76912120+Ragdha-Elgaidi@users.noreply.github.com>
2023-02-05 08:39:34 +0200

    Create Practice(13).sql

---
+++ SQL Subqueries & Temporary Tables/Practice(13).sql
@@ -0,0 +1,19 @@
+-- How many accounts had more total purchases than the account name 
+-- which has bought the most standard_qty paper throughout their lifetime as a customer? 
+WITH t1 AS (
+  SELECT a.name account_name, SUM(o.standard_qty) total_std, SUM(o.total) total
+  FROM accounts a
+  JOIN orders o
+  ON o.account_id = a.id
+  GROUP BY 1
+  ORDER BY 2 DESC
+  LIMIT 1), 
+t2 AS (
+  SELECT a.name
+  FROM orders o
+  JOIN accounts a
+  ON a.id = o.account_id
+  GROUP BY 1
+  HAVING SUM(o.total) > (SELECT total FROM t1))
+SELECT COUNT(*)
+FROM t2;
