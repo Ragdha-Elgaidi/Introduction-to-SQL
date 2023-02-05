fe50d024007f20ed8a8ff3e058f102dcb7162977
Ragdha El-gaidi <76912120+Ragdha-Elgaidi@users.noreply.github.com>
2023-02-05 08:40:08 +0200

    Create Practice(16).sql

---
+++ SQL Subqueries & Temporary Tables/Practice(16).sql
@@ -0,0 +1,14 @@
+-- What is the lifetime average amount spent in terms of total_amt_usd,
+-- including only the companies that spent more per order, on average, than the average of all orders.
+WITH t1 AS (
+   SELECT AVG(o.total_amt_usd) avg_all
+   FROM orders o
+   JOIN accounts a
+   ON a.id = o.account_id),
+t2 AS (
+   SELECT o.account_id, AVG(o.total_amt_usd) avg_amt
+   FROM orders o
+   GROUP BY 1
+   HAVING AVG(o.total_amt_usd) > (SELECT * FROM t1))
+SELECT AVG(avg_amt)
+FROM t2;
