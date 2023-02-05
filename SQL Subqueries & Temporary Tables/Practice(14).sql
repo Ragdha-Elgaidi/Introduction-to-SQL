f7c8fd2a92ed5468185aa03b4f90b8c6740ebeea
Ragdha El-gaidi <76912120+Ragdha-Elgaidi@users.noreply.github.com>
2023-02-05 08:39:55 +0200

    Create Practice(14).sql

---
+++ SQL Subqueries & Temporary Tables/Practice(14).sql
@@ -0,0 +1,16 @@
+-- For the customer that spent the most (in total over their lifetime as a customer) total_amt_usd,
+-- how many web_events did they have for each channel?
+WITH t1 AS (
+   SELECT a.id, a.name, SUM(o.total_amt_usd) tot_spent
+   FROM orders o
+   JOIN accounts a
+   ON a.id = o.account_id
+   GROUP BY a.id, a.name
+   ORDER BY 3 DESC
+   LIMIT 1)
+SELECT a.name, w.channel, COUNT(*)
+FROM accounts a
+JOIN web_events w
+ON a.id = w.account_id AND a.id =  (SELECT id FROM t1)
+GROUP BY 1, 2
+ORDER BY 3 DESC;
