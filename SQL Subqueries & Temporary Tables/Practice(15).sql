57c469eb26c3c199af4f1942a3d1e378dd91afd4
Ragdha El-gaidi <76912120+Ragdha-Elgaidi@users.noreply.github.com>
2023-02-05 08:40:00 +0200

    Create Practice(15).sql

---
+++ SQL Subqueries & Temporary Tables/Practice(15).sql
@@ -0,0 +1,11 @@
+-- What is the lifetime average amount spent in terms of total_amt_usd for the top 10 total spending accounts?
+WITH t1 AS (
+   SELECT a.id, a.name, SUM(o.total_amt_usd) tot_spent
+   FROM orders o
+   JOIN accounts a
+   ON a.id = o.account_id
+   GROUP BY a.id, a.name
+   ORDER BY 3 DESC
+   LIMIT 10)
+SELECT AVG(tot_spent)
+FROM t1;
