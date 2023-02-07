669a96d2ec8b148cfc6416a91045a4d2578caa9b
Ragdha El-gaidi <76912120+Ragdha-Elgaidi@users.noreply.github.com>
2023-02-07 15:17:08 +0200

    Create Practice(9).sql

---
+++ SQL Data Cleaning/Practice(9).sql
@@ -0,0 +1,8 @@
+-- Each company in the accounts table wants to create an email address for each primary_poc.
+-- The email address should be the first name of the primary_poc .
+-- last name primary_poc @ company name .com.
+WITH t1 AS (
+ SELECT LEFT(primary_poc,     STRPOS(primary_poc, ' ') -1 ) first_name,  RIGHT(primary_poc, LENGTH(primary_poc) - STRPOS(primary_poc, ' ')) last_name, name
+ FROM accounts)
+SELECT first_name, last_name, CONCAT(first_name, '.', last_name, '@', name, '.com')
+FROM t1;
